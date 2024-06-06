49398ada95b3e5ab1fdcc7b982e1679a8cdc99f8
Ajay-101 <ajay.patil@anuyat.com>
2024-06-06 16:19:50 +0530

    add comment

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 connectDB();
 
 app.use(express.json());
+// app.set('view engine', 'ejs');
 app.set('view engine', 'ejs');
 
 const authRoutes = require('./Server/routes/routes');
